c281d2c492968a5a0d272614f51d17fff039a4ff
angelaalaisa <70028480+angelaalaisa@users.noreply.github.com>
2022-03-01 08:59:24 -0500

    Create EmailInfoExtractor.cpp

---
+++ Homework/Homework2/EmailInfoExtractor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+class BaseEmailHeaderType {
+    public:
+        
+};
+
+class GmailHeaderType: public BaseEmailHeaderType{
+    public:
+
+};
+
+class OutlookHeaderType: public BaseEmailHeaderType{
+    public:
+
+};
